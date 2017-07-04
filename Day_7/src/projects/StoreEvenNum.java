776908b9c420b681507d34bb85cb9e9c16a0d8d0
mnimmz <nimmons92@gmail.com>
2017-07-04 17:58:13 -0400

    Day 7 homework

---
+++ Day_7/src/projects/StoreEvenNum.java
@@ -0,0 +1,18 @@
+package projects;
+
+public class StoreEvenNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		
+		int [] fifty = new int[50];                       
+		for (int a = 0; a < fifty.length;) {
+			a = a + 2;
+		    System.out.println(a);
+		}
+
+	}
+
+}
